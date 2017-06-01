344e6cebafa110cc87b8ee57a705ad01a9969d9e
Kim kyuna <kkn00077@gmail.com>
2017-06-01 19:27:54 +0900

    #5 button event programming

--- app/src/main/java/kn0077/kr/hs/emirim/simplediary/MainActivity.java
+++ app/src/main/java/kn0077/kr/hs/emirim/simplediary/MainActivity.java
@@ -1,15 +1,20 @@
 package kn0077.kr.hs.emirim.simplediary;
 
+import android.content.Context;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -26,6 +31,20 @@ public class MainActivity extends AppCompatActivity {
         date = (DatePicker) findViewById(R.id.date_pick);
         edit = (EditText) findViewById(R.id.edit);
         but = (Button) findViewById(R.id.but);
+        but.setOnClickListener(new View.OnClickListener() { // () -> 일을 시킬수있는 클래스를 매개변수로 넣어줌
+            @Override
+            public void onClick(View v) {
+                try {
+                    FileOutputStream fOut= openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str=edit.getText().toString(); // getText()가 String 형이 아닌 다른 형식으로 형반환을 하기 때문에 변환
+                    fOut.write(str.getBytes());
+                    fOut.close();
+                    Toast.makeText(MainActivity.this,"정상적으로 "+fileName+"파일이 저장되었습니다.",Toast.LENGTH_LONG).show();
+                } catch (IOException e) { // IOException이 FileNotFoundException의 부모라서 한번에 처리 가능하다
+                    e.printStackTrace();
+                }
+            }
+        });
 
         Calendar cal = Calendar.getInstance();//static메서드 캘린더(추상클래스)를 사용할 수 있도록 하는 현재객체 클래스의 참조값을 반환
         int year=cal.get(Calendar.YEAR);
